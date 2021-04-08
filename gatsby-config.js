dd502b0bb938a165ecd6fde686585e771da64988
Mario Andres <marioarendon2@gmail.com>
2021-04-08 04:28:47 -0600

    read data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    plugins: [
-
-    ]
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: 'hopkins',
+        remote: 'https://github.com/CSSEGISandData/COVID-19.git',
+        branch: 'master',
+        patterns: `csse_covid_19_data/csse_covid_19_time_series/*.csv`,
+        local: `${__dirname}/hopkins/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/hopkins/csse_covid_19_data/csse_covid_19_time_series/`,
+      },
+    },
+    `gatsby-transformer-csv`,
+  ],
+};
