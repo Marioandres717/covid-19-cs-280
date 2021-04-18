dff3e3452a23a501be270e22b2dfabc7a360eec1
Mario Andres <marioarendon2@gmail.com>
2021-04-18 15:08:47 -0600

    remove unused data

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,11 +11,7 @@ module.exports = {
         name: 'hopkins',
         remote: 'https://github.com/CSSEGISandData/COVID-19.git',
         branch: 'master',
-        patterns: [
-          `csse_covid_19_data/csse_covid_19_time_series/*.csv`,
-          `csse_covid_19_data/csse_covid_19_daily_reports/*.csv`,
-          // `csse_covid_19_data/csse_covid_19_daily_reports_us/*.csv`,
-        ],
+        patterns: [`csse_covid_19_data/csse_covid_19_daily_reports/*.csv`],
         local: `${__dirname}/hopkins/`,
       },
     },
